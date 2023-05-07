a3cae6b970e4603ec419b2e850e2042a4c7cd1df
Som Akudu <somakudu@gmail.com>
2023-05-07 19:00:37 +0100

    first line of code in python

--- 013_add_two.py
+++ 013_add_two.py
@@ -5,6 +5,10 @@ from lib.helpers import check_that_these_are_equal
 # Now you try. Here's an exercise for you:
 #
 # @TASK: Write a function called `add_two` that:
+
+def add_two(num):
+    return num + 2
+
 #
 # * Takes a number as input
 # * Adds two to it
